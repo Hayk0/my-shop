433cceb084c8337ec12dc336edb1533f5143f5b5
Hayk Sargsyan <hayko157@gmail.com>
2019-02-17 00:10:40 +0400

    add product

--- app/components/shop/add-product.js
+++ app/components/shop/add-product.js
@@ -17,17 +17,36 @@ export default Component.extend({
         changeEditMood() {
             this.toggleProperty('editMood');
         },
-        addNewShop(shopId) {
-            const self = this;
-            const currentShop = self.get('store').peekRecord('shop', shopId);
-            currentShop.products.pushObject(self.store.createRecord('product', {
+        addNewShop(model) {
+            const self = this
+            let newProduct = this.get('store').createRecord('product', {
                 name: self.get('name'),
                 qty: self.get('qty'),
                 price: self.get('price'),
-            }))
+                shop: model
+            });
+            const currentShop = self.get('store').peekRecord('shop', model.id);
+            currentShop.products.pushObject(self.store.createRecord('product', newProduct));
+            newProduct.save()
+                .then(() => {
+                    // this.get('model.products').pushObject(newProduct);
+                    
+                    // console.log(this.get('model.products'))
+                    // this.set('newName', '');
+                    // this.set('qty', 0);
+                    // this.set('price', 0);
+                    // this.set('showForm', false)
+                })
+            // const self = this;
+            // const currentShop = self.get('store').peekRecord('shop', shopId);
+            // currentShop.products.pushObject(self.store.createRecord('product', {
+            //     name: self.get('name'),
+            //     qty: self.get('qty'),
+            //     price: self.get('price'),
+            // }))
             // currentShop.save(res => console.log(res.products.get('firstObject')) )
 
-            console.log(currentShop.products)
+            console.log(model.id, model.name);
         }
     }
 });
