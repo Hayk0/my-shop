205a9572cd2b18971e342b3b4725cbd64c36ee7a
Hayk Sargsyan <hayko157@gmail.com>
2019-02-17 21:31:40 +0400

    completed

--- app/components/shop/add-product.js
+++ app/components/shop/add-product.js
@@ -1,52 +1,51 @@
 import Component from '@ember/component';
-import { empty } from '@ember/object/computed';
+import { empty, or } from '@ember/object/computed';
 import { inject as service } from '@ember/service';
 
 export default Component.extend({
-    store : service(),
-    editMood : false,
-    name : '',
-    qty : '',
-    price : '',
-    disabled :  empty('name') && empty('qty') && empty('price'),
-    success : false,
-    successMessage : 'Product was added successfully!',
-    error: false,
-    errorMessage: 'Error, try again!',
-    actions : {
-        changeEditMood() {
-            this.toggleProperty('editMood');
-        },
-        addNewShop(model) {
-            const self = this
-            let newProduct = this.get('store').createRecord('product', {
-                name: self.get('name'),
-                qty: self.get('qty'),
-                price: self.get('price'),
-                shop: model
-            });
-            const currentShop = self.get('store').peekRecord('shop', model.id);
-            currentShop.products.pushObject(self.store.createRecord('product', newProduct));
-            newProduct.save()
-                .then(() => {
-                    // this.get('model.products').pushObject(newProduct);
-                    
-                    // console.log(this.get('model.products'))
-                    // this.set('newName', '');
-                    // this.set('qty', 0);
-                    // this.set('price', 0);
-                    // this.set('showForm', false)
-                })
-            // const self = this;
-            // const currentShop = self.get('store').peekRecord('shop', shopId);
-            // currentShop.products.pushObject(self.store.createRecord('product', {
-            //     name: self.get('name'),
-            //     qty: self.get('qty'),
-            //     price: self.get('price'),
-            // }))
-            // currentShop.save(res => console.log(res.products.get('firstObject')) )
-
-            console.log(model.id, model.name);
-        }
-    }
+  store : service(),
+  editMood : false,
+  name : '',
+  qty : '',
+  price : '',
+  disabled :  empty('name') && empty('qty') && empty('price'),
+  success : false,
+  error: false,
+  notification: '',
+  showNotification: or('success', 'error'),
+  actions : {
+    changeEditMood() {
+      this.toggleProperty('editMood');
+    },
+    addNewShop(model) {
+      const self = this
+      let newProduct = self.get('store').createRecord('product', {
+        name: self.get('name'),
+        qty: Math.abs(self.get('qty')),
+        price: Math.abs(self.get('price')),
+        shop: model
+      });
+      const currentShop = self.get('store').peekRecord('shop', model.id);
+      newProduct.save()
+        .then(() => {
+          currentShop.products.pushObject(self.store.createRecord('product', newProduct));
+          self.setProperties({
+            success: true,
+            name : '',
+            qty: '',
+            price: '',
+            editMood: false,
+            notification: 'Product was added successfully',
+          });
+          setTimeout(() => self.toggleProperty('success'), 2000);
+        })
+        .catch(() => {
+          self.setProperties({
+            error: true,
+            notification: 'Error, try again!'
+          });
+          setTimeout(() => self.toggleProperty('error'), 2000);
+        });
+  }
+  }
 });
--- app/models/product.js
+++ app/models/product.js
@@ -1,9 +1,11 @@
 import DS from 'ember-data';
 const {attr, belongsTo, Model} = DS;
+import { computed } from '@ember/object';
+
 
 export default Model.extend({
-    name: attr('string'),
-    qty: attr('number'),
-    price: attr('number'),
-    shop: belongsTo('shop')
+  name: attr('string'),
+  qty: attr('number', { defaultValue: 0 }),
+  price: attr('number', { defaultValue: 0 }),
+  shop: belongsTo('shop'),
 });
